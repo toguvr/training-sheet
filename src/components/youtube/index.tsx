86303ed50760428b9bc90812e130c582ed76ab94
toguvr <augustotf93@gmail.com>
2023-05-18 12:09:28 -0300

    fix(width)

--- src/components/youtube/index.tsx
+++ src/components/youtube/index.tsx
@@ -9,8 +9,6 @@ const VideoEmbed = ({ url }: { url: any }) => {
   return (
     <div className="video-container">
       <iframe
-        width="560"
-        height="315"
         src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
